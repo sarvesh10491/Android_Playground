8ff3bb3af2ff94ff8f6ef73409ae3a3ca0b5f974
sarvesh10491 <sarvesh10491@gmail.com>
2020-10-10 17:23:36 -0700

    Added LogMe

---
+++ LogMe/LogMe_App/app/src/main/java/com/example/logme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.logme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+    }
+}
